8233825f4ba839eef17a94feb94a85efd999ca60
Lee-Margo <eaten1216@gmail.com>
2024-03-03 00:03:25 +0800

    shopping list first commit

---
+++ shopping-cart/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="bg-black text-white">
+    <div class="wrapper">
+      <nav>
+        <RouterLink class="mr-4" to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
